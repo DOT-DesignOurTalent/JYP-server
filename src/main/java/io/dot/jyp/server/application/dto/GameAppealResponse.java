ee8cc245efa898648e207a5306fc053377423bdc
Ethan <jmeef0802@gmail.com>
2021-03-13 18:24:15 +0900

    chore: lint

--- src/main/java/io/dot/jyp/server/application/dto/GameAppealResponse.java
+++ src/main/java/io/dot/jyp/server/application/dto/GameAppealResponse.java
@@ -1,13 +1,5 @@
 package io.dot.jyp.server.application.dto;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GameAppealResponse {
 
 }
